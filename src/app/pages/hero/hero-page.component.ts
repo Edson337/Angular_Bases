77bd3734e53a384c0789bbad724927f4baacf931
Edson733 <20213tn034@utez.edu.mx>
2025-09-26 18:58:36 -0600

    Señales computadas

--- src/app/pages/hero/hero-page.component.ts
+++ src/app/pages/hero/hero-page.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
-    templateUrl: './hero-page.component.html'
+    templateUrl: './hero-page.component.html',
+    imports: [UpperCasePipe],
 })
 export class HeroPageComponent {
     name = signal('Ironman');
-    age = signal(35);
+    age = signal(45);
 
-    getHeroDescription() {
-        return `${this.name()} tiene ${this.age()} años.`;
-    }
+    heroDescription = computed(() => {
+        const description = `${this.name()} tiene ${this.age()} años.`;
+        return description;
+    })
+
+    capitalizedName = computed(() => this.name().toUpperCase());
+
+    // getHeroDescription() {
+    //     return `${this.name()} tiene ${this.age()} años.`;
+    // }
 
     changeHero() {
         this.name.set('Spiderman');
