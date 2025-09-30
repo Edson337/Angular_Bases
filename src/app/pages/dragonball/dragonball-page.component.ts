8da272489078a8a68f92c7b8337ccbb2dad09e78
Edson733 <20213tn034@utez.edu.mx>
2025-09-30 17:43:00 -0600

    @for - Realizar iteraciones de elementos

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,9 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+
+interface Character {
+    id: number;
+    name: string;
+    power: number;
+}
 
 @Component({
     imports: [],
     templateUrl: './dragonball-page.component.html'
 })
 export class DragonBallPageComponent {
-
+    characters = signal<Character[]>([
+        { id: 1, name: 'Goku', power: 15000 },
+        { id: 2, name: 'Vegeta', power: 12000 },
+        { id: 3, name: 'Trunks', power: 8000 },
+        { id: 4, name: 'Gohan', power: 9500 },
+    ])
 }
