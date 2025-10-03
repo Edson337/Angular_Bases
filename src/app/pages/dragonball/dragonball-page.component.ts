d7ae973e732ecfc8e588879867fc35af23f00c2f
Edson733 <20213tn034@utez.edu.mx>
2025-10-03 17:06:20 -0600

    Inputs - Agregar personaje

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -14,6 +14,9 @@ interface Character {
     templateUrl: './dragonball-page.component.html'
 })
 export class DragonBallPageComponent {
+    name = signal('');
+    power = signal(0);
+
     characters = signal<Character[]>([
         { id: 1, name: 'Goku', power: 15000 },
         { id: 2, name: 'Vegeta', power: 12000 },
@@ -27,4 +30,22 @@ export class DragonBallPageComponent {
     //         'text-danger': true,
     //     }
     // })
+
+    addCharacter() {
+        if(!this.name() || !this.power() || this.power() <= 0) return;
+
+        const newCharacter: Character = {
+            id: this.characters().length + 1,
+            name: this.name(),
+            power: this.power()
+        }
+
+        this.characters.update(list => [...list, newCharacter]);
+        this.resetFields();
+    }
+
+    resetFields() {
+        this.name.set('');
+        this.power.set(0);
+    }
 }
